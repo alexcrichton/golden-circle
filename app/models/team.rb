e68b9c1daf15fef18a95f76fcaff1a8957667198
Max Hawkins <maxhawkins@gmail.com>
2009-01-09 15:44:10 -0600

    Added some session specs. Got rid of -all- restful_auth stuff. Lots of failing specs

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,13 +1,8 @@
 class Team < ActiveRecord::Base
   
-  include Authentication
-  include Authentication::ByPassword
-  include Authentication::ByCookieToken
-  
   validates_presence_of     :contact_email
   validates_uniqueness_of   :contact_email
   validates_length_of       :contact_email,    :within => 6..100 #r@a.wk
-  validates_format_of       :contact_email,    :with => Authentication.email_regex, :message => Authentication.bad_email_message
   
   attr_protected :admin
   
