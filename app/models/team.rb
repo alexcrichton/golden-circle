d2db5a681cd87a70355a25cf0a70c23434f4b9e6
Max Hawkins <maxhawkins@gmail.com>
2009-01-09 22:48:27 -0600

    Added validation that the deadline has not passed

--- app/models/team.rb
+++ app/models/team.rb
@@ -2,6 +2,8 @@ class Team < ActiveRecord::Base
   
   acts_as_authentic
   
+  validate :submitted_before_deadline?
+  
 #  validates_presence_of :school_name
 #  validates_presence_of :contact_name
 #  validates_presence_of :contact_phone
@@ -15,5 +17,13 @@ class Team < ActiveRecord::Base
   has_many :proctors, :attributes => true, :discard_if => proc { |proctor| proctor.name.blank? }
   has_many :apprentices, :class_name => 'Student', :conditions => ['level = ?', Student::APPRENTICE], :attributes => true, :discard_if => :blank?
   has_many :wizards,     :class_name => 'Student', :conditions => ['level = ?', Student::WIZARD], :attributes => true, :discard_if => :blank?
+  
+  private
+  def submitted_before_deadline?
+    # Needs to be before midnight on Tuesday, January 27, 2009
+    if Time.zone.now > Time.zone.local(2009, 1, 27, 24, 0, 0)
+      errors.add(:end_time, "The registration deadline has passed. Please bring your changes to the registration table the night of the event")
+    end
+  end
   
 end
