5811d60d935b83276d10c4c8a46cc707e2f74529
Max Hawkins <maxhawkins@gmail.com>
2009-01-09 19:24:09 -0600

    Spec'd some teams

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,11 +1,9 @@
 class TeamsController < ApplicationController
   
-  before_filter :load_team
   before_filter :ensure_owner, :except => [:create]
   before_filter :ensure_admin, :only => [:index]
   
   # GET /teams
-  # GET /teams.xml
   def index
     @teams = Team.find(:all, :order => 'enrollment DESC')
 
@@ -16,35 +14,24 @@ class TeamsController < ApplicationController
   end
 
   # GET /teams/1
-  # GET /teams/1.xml
   def show
-    render :action => 'edit'
-#    @team = Team.find(params[:id])
-#
-#    respond_to do |format|
-#      format.html # show.html.erb
-#      format.xml  { render :xml => @team }
-#    end
+    @team = Team.find(params[:id])
+
+    respond_to do |format|
+      format.html
+    end
   end
 
   # GET /teams/new
-  # GET /teams/new.xml
- def new
-   @team = Team.new
+  def new
+    @team = Team.new
  
-   respond_to do |format|
-     format.html # new.html.erb
-     format.xml  { render :xml => @team }
-   end
- end
-
-  # GET /teams/1/edit
-#  def edit
-#    @team = Team.find(params[:id])
-#  end
+    respond_to do |format|
+      format.html
+    end
+  end
 
   # POST /teams
-  # POST /teams.xml
   def create
     @team = Team.new(params[:team])
 
@@ -52,65 +39,45 @@ class TeamsController < ApplicationController
       if @team.save
         flash[:notice] = 'Team was successfully created.'
         format.html { redirect_to(@team) }
-        format.xml  { render :xml => @team, :status => :created, :location => @team }
       else
         format.html { render :action => "new" }
-        format.xml  { render :xml => @team.errors, :status => :unprocessable_entity }
       end
+    end
+  end
+  
+  # GET /teams/1/edit
+  def edit
+    @team = Team.find(params[:id])
+    
+    respond_to do |format|
+      format.html
     end
   end
 
   # PUT /teams/1
-  # PUT /teams/1.xml
   def update
-    params[:proctors].each_pair do |id, hash|
-      if id == '0'
-        team.proctors << Proctor.new(hash)
-      else
-        p = Proctor.find(id)
-        p.update_attributes(hash)
-        p.destroy if hash[:name].blank?
-      end
-    end
-    
-    params[:students].each_pair do |id, hash|
-      if id == '0'
-        team.students << Student.new(hash)
-      else
-        s = Student.find(id)
-        s.update_attributes(hash)
-        s.destroy if hash[:first_name].blank?
-      end
-    end
+    @team = Team.find(params[:id])
     
     respond_to do |format|
       if @team.update_attributes(params[:team])
         flash[:notice] = 'Team was successfully updated.'
         format.html { redirect_to(@team) }
-        format.xml  { head :ok }
       else
         format.html { render :action => "edit" }
-        format.xml  { render :xml => @team.errors, :status => :unprocessable_entity }
       end
     end
   end
 
   # DELETE /teams/1
-  # DELETE /teams/1.xml
   def destroy
     @team.destroy
     
     respond_to do |format|
       format.html { redirect_to(teams_url) }
-      format.xml  { head :ok }
     end
   end
   
   protected
-  def load_team
-    @team = Team.find(params[:id]) if params[:id]
-  end
-  
   def ensure_owner
     if(@team.nil? || current_team.nil? || (current_team.id != @team.id && !current_team.admin))
       # redirect_to(denied_path)
@@ -121,5 +88,8 @@ class TeamsController < ApplicationController
     if(current_team.nil? || !current_team.admin)
       # redirect_to(denied_path)
     end
+  end
+  
+  def ensure_admin
   end
 end
