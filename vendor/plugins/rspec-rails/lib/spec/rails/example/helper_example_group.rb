43a981c25f2c80d6a9cbacd1fa29d12a1e34b285
Alex Crichton <alex@alex-arch.domain.org>
2009-01-10 16:36:24 -0600

    Braid: Updated mirror 'vendor/plugins/rspec-rails' to '1a09ad4'

--- vendor/plugins/rspec-rails/lib/spec/rails/example/helper_example_group.rb
+++ vendor/plugins/rspec-rails/lib/spec/rails/example/helper_example_group.rb
@@ -26,6 +26,8 @@ module Spec
       #     end
       #   end
       class HelperExampleGroup < FunctionalExampleGroup
+        attr_accessor :output_buffer
+        
         class HelperObject < ActionView::Base
           def protect_against_forgery?
             false
@@ -117,7 +119,9 @@ module Spec
 
           @flash = ActionController::Flash::FlashHash.new
           session['flash'] = @flash
-
+          
+          @output_buffer = ""
+          @template = helper
           ActionView::Helpers::AssetTagHelper::reset_javascript_include_default
           
           helper.session = session
