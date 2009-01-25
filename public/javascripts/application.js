076bc35f9ac7a00b5afb898c489f8319ca022210
Alex Crichton <adcrichton@gmail.com>
2009-01-25 12:53:24 -0600

    various changes to style, focusing on page loading is given to first input on the page unless overriden

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,14 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+var input;
+function focusFirstInput() {
+	var inputs = document.getElementsByTagName("input");
+	for ( var i = 0; i < inputs.length; i++)
+		if (inputs[i].type != "hidden") {
+			input = inputs[i];
+			break;
+		}
+	if (input != null)
+		input.focus();
+}
