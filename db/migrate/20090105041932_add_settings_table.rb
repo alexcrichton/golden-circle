60a67c92d24689a8cf05133c63ee55ad7cb9afa4
Alex Crichton <adcrichton@gmail.com>
2009-03-04 22:46:14 -0600

    forgot the migration...

---
+++ db/migrate/20090105041932_add_settings_table.rb
@@ -0,0 +1,22 @@
+class AddSettingsTable < ActiveRecord::Migration
+  def self.up
+    create_table :settings, :force => true do |t|
+      t.string :var, :null => false
+      t.text   :value, :null => true
+      t.timestamps
+    end
+    Settings.max_student_score = 25
+    Settings.large_school_cutoff = 200
+    Settings.max_students_on_team = 15
+    Settings.max_team_score = 30
+    Settings.test_scores_to_count = 5
+    Settings.team_test_points_per_question = 5
+    Settings.cost_per_student = 4
+    Settings.deadline = Time.zone.local(2009, 2, 24, 24, 0, 0)
+  end
+
+  def self.down
+    drop_table :settings
+  end
+
+end
