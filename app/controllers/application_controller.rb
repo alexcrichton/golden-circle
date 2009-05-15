1f4d3d60fdebdaa70f801cfed12a4bc9191b123c
Alex Crichton <alexc605@gmail.com>
2009-05-14 23:36:03 -0500

    not sure what these are... but they're here

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@
 
 class ApplicationController < ActionController::Base
 
-  filter_parameter_logging :password, :password_confirmation
+  filter_parameter_logging :password, :password_confirmation, :perishable_token, :persistence_token
 
   include ApplicationHelper
 
@@ -12,10 +12,5 @@ class ApplicationController < ActionController::Base
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
   protect_from_forgery # :secret => '2bc5e62fe0ec71976a2d8a0bc2328c5b'
-
-  # See ActionController::Base for details
-  # Uncomment this to filter the contents of submitted sensitive data parameters
-  # from your application log (in this case, all fields with names like "password").
-  # filter_parameter_logging :password
 
 end
