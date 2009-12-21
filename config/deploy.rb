66cd76ea59d0a76c89a952b54375c2a47be177df
Alex Crichton <alex@alexcrichton.com>
2009-12-21 03:17:51 -0600

    Don't force create

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,30 +1,23 @@
-require 'erb'
-
-set :web_server, "goldencircle.dyndns.org"
+set :web_server, "10.10.10.113"
 
 role :app, web_server
 role :web, web_server
 role :db,  web_server, :primary => true
-
-set :application, "goldencircle"
 
 set :scm, :git
 set :repository, "git://github.com/alexcrichton/golden-circle.git"
 set :branch, "master"
 set :deploy_via, :remote_cache
 
+set :rake, "/opt/ruby1.8/bin/rake"
 
 set :user, "capistrano"
 set :use_sudo, false
 
-# If you aren't deploying to /u/apps/#{application} on the target
-# servers (which is the default), you can specify the actual location
-# via the :deploy_to variable:
-set :deploy_to, "/srv/www/#{application}"
+set :deploy_to, "/srv/http/goldencircle"
 
 before "deploy:setup", :db
 after "deploy:update_code", "db:symlink"
-after "deploy:update_code", "deploy:create_assets"
 
 namespace :db do
   task :default do
@@ -34,7 +27,7 @@ namespace :db do
   desc "Make symlink for database yaml"
   task :symlink do
     run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
-    run "ln -s #{shared_path}/files #{latest_release}/public/files"
+    run "ln -nfs #{shared_path}/files #{latest_release}/public/files"
   end
 
 end
@@ -47,11 +40,5 @@ namespace :deploy do
   end
   task :stop, :roles => :app do
     # Do nothing, don't want to kill nginx
-  end
-  desc "Create asset packages for production"
-  task :create_assets, :roles => :app do
-    run <<-EOF
-     cd #{release_path} && rake asset:packager:build_all
-    EOF
   end
 end
