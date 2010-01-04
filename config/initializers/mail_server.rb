2782c7aa9b6e7060c1c69a6a39c304f38a7bdd13
Alex Crichton <alex@alexcrichton.com>
2010-01-04 03:30:45 -0600

    Better support for that mailing stuff

--- config/initializers/mail_server.rb
+++ config/initializers/mail_server.rb
@@ -4,9 +4,10 @@ ActionMailer::Base.smtp_settings = {
    :address => "smtp.gmail.com",
    :port => 587,
    :domain => "gmail.com",
-   :authentication => :login,
+   :authentication => :plain,
    :user_name => ENV['USER_EMAIL'],
    :password =>  ENV['USER_EMAIL_PASSWORD'],
+   :enable_starttls_auto => true
 }
 
 if Rails.env.production?
