78cedf27ff4c45b48aafee45a2f18ff6923c6da5
Alex Crichton <alex@alexcrichton.com>
2010-01-03 22:49:20 -0600

    Make work with gmail

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -2,7 +2,7 @@ class Notification < ActionMailer::Base
 
   def password_reset_instructions(school)
     subject       "Password Reset Instructions"
-    from          "Golden Circle Notifier <golden.circle.contest@gmail.com>"
+    from          "golden.circle.contest@gmail.com"
     recipients    school.email
     sent_on       Time.now
     body          :edit_password_reset_url => edit_password_reset_url(school.perishable_token)
@@ -11,7 +11,7 @@ class Notification < ActionMailer::Base
   def confirmation(school)
     subject        "Golden Circle Contest Confirmation of Registration"
     recipients     school.email
-    from           'Golden Circle Notifier <golden.circle.contest@gmail.com>'
+    from           'golden.circle.contest@gmail.com'
     sent_on        Time.now
     body           :school => school
   end
