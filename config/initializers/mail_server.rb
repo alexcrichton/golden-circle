62563b0f38773b5e4782fc2f2ae6f7669ea48283
Alex Crichton <alex@alexcrichton.com>
2010-01-03 22:42:45 -0600

    Some mail changes

--- config/initializers/mail_server.rb
+++ config/initializers/mail_server.rb
@@ -1,11 +1,16 @@
-ActionMailer::Base.raise_delivery_errors = false
 ActionMailer::Base.default_content_type = 'text/html'
-ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.delivery_method = :smtp unless Rails.env.test?
 ActionMailer::Base.smtp_settings = {
    :address => "smtp.gmail.com",
    :port => 587,
    :domain => "gmail.com",
-   :authentication => :plain,
-   :user_name => "golden.circle.contest",
-   :password => "newtonphi",
+   :authentication => :login,
+   :user_name => ENV['USER_EMAIL'],
+   :password =>  ENV['USER_EMAIL_PASSWORD'],
 }
+
+if Rails.env.production?
+  ActionMailer::Base.default_url_options[:host] = 'goldencircle.heroku.com'
+else
+  ActionMailer::Base.default_url_options[:host] = 'localhost'
+end
